99940803b72a83935e690301cbea82af5f863245
Victor <victorgdl89@gmail.com>
2016-11-12 19:00:42 -0600

    user guess update

--- bagel.js
+++ bagel.js
@@ -18,22 +18,44 @@
 
 
 var compChoice1, compChoice2, compChoice3;
+
 function getRandomNum() {
 
 	return Math.floor(Math.random() *10)
 }
 
-
 function startGame() {
 	//choose 3 unique numbers
 
 	do{
+		var computerNum = [compChoice1, compChoice2, compChoice3];
 		compChoice1 = getRandomNum();
 		compChoice2 = getRandomNum();
 		compChoice3 = getRandomNum();
 	}while (compChoice1 === compChoice2 || compChoice1 === compChoice3 || compChoice2=== compChoice3);
 
-}
+};
+
 startGame();
-var computerNum = [compChoice1, compChoice2, compChoice3];
-alert(computerNum);
+ var computerNum = [compChoice1, compChoice2, compChoice3];
+ alert("The computer has choosen, enter your guess");
+
+
+
+
+
+// function getUserGuess(){
+// 	var playerNum = [guessDigit1, guessDigit2, guessDigit3];
+//
+//   document.getElementById("guessBtn").addEventListener.("click", function(e){
+//    e.preventDefault();
+// 	 guessDigit1 = document.getElementById("guessNum1").value;
+// 	 guessDigit2 = document.getElementById("guessNum2").value;
+// 	 guessDigit3 = document.getElementById("guessNum3").value;
+// alert(playerNum)};
+// matchGuess();
+//
+// guessDigit1 === compChoice1
+// guessDigit1===compChoice1
+// guessDigit1===compChoice1
+// });
