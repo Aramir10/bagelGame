8352f26180b66542d3b6384894fc3487f2ca7feb
Victor <victorgdl89@gmail.com>
2016-11-17 10:18:49 -0600

    user guess function update

--- bagel.js
+++ bagel.js
@@ -1,4 +1,3 @@
-
 //create a random number for the computer decision
 //pull our numbers from text
 // validate agaist what number the computer is thinking of
@@ -9,40 +8,41 @@
 // var compChoice3 = Math.floor(Math.random() * 10);
 // var computerNum = [compChoice1, compChoice2, compChoice3];
 // alert(computerNum);
-confirm("Are you ready to play Bagels?");
+
 // global
+function getRandomNum() {
+    return Math.floor(Math.random() * 10)
+}
+
+
 var compChoice1, compChoice2, compChoice3;
 var number1, number2, number3;
 
-function getRandomNum() {
-    return Math.floor(Math.random() * 10)
-    }
+
 function startGame() {
-    //here the computer will choose 3 unique numbers creating Arrray
     do {
-        var computerNum = [compChoice1, compChoice2, compChoice3];
         compChoice1 = getRandomNum();
         compChoice2 = getRandomNum();
         compChoice3 = getRandomNum();
-    } while (compChoice1 == compChoice2 || compChoice1 == compChoice3 || compChoice2 == compChoice3);
     }
+    while (compChoice1 == compChoice2 || compChoice1 == compChoice3 || compChoice2 == compChoice3);
+}
 
-startGame()
-//Here will get the users input values
-document.getElementById('addForm').addEventListener('submit', function(e) {
-            e.preventDefault();
-            number1 = document.getElementById('guessNum1').value;
-            number2 = document.getElementById('guessNum2').value;
-            number3 = document.getElementById('guessNum3').value;
-            var computerNum = [compChoice1, compChoice2, compChoice3];
-            var userGuess = [number1, number2, number3]
-            alert('Your number Guess is  ' + userGuess + "And the computer guess   " + computerNum);
-            console.log(computerNum);
-						// compare values
-            });
+document.getElementById('startGame').addEventListener('click', function() {
+    startGame();
+    console.log();
+});
+
 
 
 function compareUsrvsCom() {
+  //Here will get the users input values
+  document.getElementById('addForm').addEventListener('submit', function(e) {
+      e.preventDefault();
+      number1 = document.getElementById('guessNum1').value;
+      number2 = document.getElementById('guessNum2').value;
+      number3 = document.getElementById('guessNum3').value;
+      alert('Your number Guess is  ' +  + "And the computer guess   " + )});
 
     var response = "";
 
@@ -56,17 +56,15 @@ function compareUsrvsCom() {
     else if (number3 == compChoice1 || number3 == compChoice2) response += "fermi  ";
 
     if (number1 === compChoice1 && number2 === compChoice2 && number3 === compChoice3) response = "You win";
-     else return "";
+    else return "";
     return response;
-		console.log(response);
-		document.getElementById("guessButn").addEventListener("click", function(){
-		document.getElementById("demoBagel").innerHTML = response;
-		});
+    console.log(response);
+    document.getElementById("guessButn").addEventListener("click", function() {
+        document.getElementById("demoBagel").innerHTML = response;
+        console.log(compareUsrvsCom());
+    });
 }
 
-
-
-console.log(compareUsrvsCom())
 // function getUserGuess(){
 // 	var playerNum = [guessDigit1, guessDigit2, guessDigit3];
 //
