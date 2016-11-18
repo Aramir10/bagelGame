6ec10e6b80143ec0834b9198782be1d7ecd40e1c
Victor <victorgdl89@gmail.com>
2016-11-17 22:44:36 -0600

    you win!

--- bagel.js
+++ bagel.js
@@ -15,11 +15,7 @@ function startGame(){
     }
     while (compChoice1 == compChoice2 || compChoice1 == compChoice3 || compChoice2 == compChoice3);
 }
-//Start game Btn
-document.getElementById('startGame').addEventListener('click', function() {
-    startGame();
-    console.log(compChoice1,compChoice2, compChoice3);
-});
+
 
 //here we'll get the usr Numnber
 function userNum(){
@@ -33,25 +29,35 @@ function compareUsrvsCom(){
 
     var response = "";
 
+
     if (number1 == compChoice1) response += "pico  ";
     else if (number1 == compChoice2 || number1 == compChoice3) response += "fermi  ";
 
+
     if (number2 == compChoice2) response += "pico  ";
-    else if (number2 == compChoice1 || compChoice2 == compChoice3) response += "fermi  ";
+    else if (number2 == compChoice1 || number2 == compChoice3) response += "fermi  ";
+
 
     if (number3 == compChoice3) response += "pico  ";
     else if (number3 == compChoice1 || number3 == compChoice2) response += "fermi  ";
 
-    if (number1 === compChoice1 && number2 === compChoice2 && number3 === compChoice3) response = "You win";
-    else return response;
+
+    if (number1 === compChoice1 && number2 === compChoice2 && number3 === compChoice3) response += "You win";
+    else if (response == "" ) return ("beagls   ")
+
+
+     return response;
     // return response;
-
    }
-
+   //Start game Btn
+   document.getElementById('startGame').addEventListener('click', function() {
+       startGame();
+       console.log(compChoice1,compChoice2, compChoice3);
+   });
 
    document.getElementById("guessButn").addEventListener("click", function() {
      userNum();
      compareUsrvsCom();
      console.log(compareUsrvsCom());
-       document.getElementById("demoBagel").innerHTML += compareUsrvsCom();
+       document.getElementById("demoBagel").innerHTML = compareUsrvsCom();
      });
